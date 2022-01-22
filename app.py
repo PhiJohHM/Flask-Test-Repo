ca3c79774993bd1e6ef603c739ddd70bd6f61f02
Philipp John <phjo@gmx.de>
2022-01-22 01:11:20 +0100

    change app.py

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT  #Flask JWT:  Json  Web Tok -> encoding Data
+
+from db import db
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.secret_key = "philipp"
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+# Add endpoints/resources
+jwt = JWT(app, authenticate, identity) # creates new endpoint: /
+api.add_resource(Store,'/store/<string:name>') 
+api.add_resource(Item,'/item/<string:name>') 
+api.add_resource(ItemList,'/items')
+api.add_resource(StoreList,'/stores')
+api.add_resource(UserRegister, '/register')
+
+if __name__ == "__main__":
+    db.init_app(app)
+    app.run(port=5000, debug=True)
+
+#Status Codes
+# 200 Ok
+# 201 Created (succesfully)
+# 202 Accepted
+# 400 bad request
+# 404 Error Code
+
